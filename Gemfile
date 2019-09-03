727c94c11f0a0214e7c3de1b84dccc6a7e2dc3f9
Argiris <anastasakis.an@gmail.com>
2019-09-03 21:59:34 +0300

    integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'friendly_id', '~> 5.2.4'
 gem 'devise', '~> 4.6', '>= 4.6.2'
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
+
+gem 'devcamp_view_tool', git: 'https://github.com/jordanhudgens/devcamp_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    DevcampViewTool::Renderer.copyright "Test", "All rights reserved"
+  end
 end
